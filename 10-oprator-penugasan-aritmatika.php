8f82e55f8b3328c414bf3247f5218c52d9ec093c
paesal240604049-netizen <paesal.240604049@student.hamzanwadi.ac.id>
2026-04-18 16:31:31 +0800

    Add files via upload

---
+++ 10-oprator-penugasan-aritmatika.php
@@ -0,0 +1,10 @@
+<?php
+
+$var1 = 10;
+$var2 = 10;
+
+//penjumlahan
+$hasil = $var1 + $var2;
+var_dump($hasil);   
+$var1 += $var2;
+var_dump($var1);
---
+++ 9-oprator-aritmatika.php
@@ -0,0 +1,15 @@
+<?php
+// oprator aritmatika
+$var1 = 10;
+$var2 = 20;
+
+//penjumlahan
+var_dump($var1 + $var2);
+
+//pengurangan
+var_dump($var1 - $var2);
+
+//perkalian
+var_dump($var1 * $var2);
+//pembagian
+var_dump($var1 / $var2);
